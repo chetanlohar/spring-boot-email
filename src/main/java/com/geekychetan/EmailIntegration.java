e6f90ce78c721a67a6be4590b9358096fbd5417a
Chetan H Lohar <chetanlohar@outlook.com>
2020-04-12 13:54:43 +0530

    Added FreeMarker for email templating.
    Signed-off-by: Chetan Lohar <chetan.lohar@enterprisedb.com>

--- src/main/java/com/geekychetan/EmailIntegration.java
+++ src/main/java/com/geekychetan/EmailIntegration.java
@@ -1,11 +1,23 @@
 package com.geekychetan;
 
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class EmailIntegration implements CommandLineRunner {
@@ -13,12 +25,28 @@ public class EmailIntegration implements CommandLineRunner {
     @Autowired
     private JavaMailSender sender;
 
-    void send(){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("chetanlohar@outlook.com");
-        message.setSubject("EmailIntegration test");
-        message.setText("This is test automated email message.");
-        sender.send(message);
+    @Autowired
+    private Configuration fmConfiguration;
+
+    void send() throws IOException, TemplateException, MessagingException {
+        Map<String, String > model = new HashMap<>();
+        model.put("firstName","Chetan");
+        model.put("lastName","Lohar");
+
+        String content = FreeMarkerTemplateUtils.processTemplateIntoString(fmConfiguration.getTemplate("email-template.txt"),model);
+
+        MimeMessage mimeMessage = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setTo("chetan.lohar@enterprisedb.com");
+        helper.setSubject("Email Integration Test");
+        helper.setText(content,true);
+
+        Resource resource = new ClassPathResource("docker.jpeg");
+        File file = new File(resource.getURI());
+
+        helper.addAttachment("docker.jpeg",file);
+        sender.send(helper.getMimeMessage());
     }
 
     @Override
